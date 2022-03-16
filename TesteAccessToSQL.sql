7d08e1280937b8af142d6bf68ce3d4550807882d
Arlen POSSAMAI <77725374+arlendev@users.noreply.github.com>
2022-03-16 19:43:10 +0100

    up

--- TesteAccessToSQL.sql
+++ TesteAccessToSQL.sql
@@ -9,7 +9,7 @@ USE `testeaccess`;
 DROP TABLE IF EXISTS `Clientes`;
 
 CREATE TABLE `Clientes` (
-  `Código Cliente` INTEGER NOT NULL, 
+  `Código Cliente` INTEGER PRIMARY KEY NOT NULL, 
   `Empresa` VARCHAR(150) NOT NULL, 
   `Status` VARCHAR(10) DEFAULT 'A', 
   `Endereço p/ Faturamento` VARCHAR(150), 
@@ -31,18 +31,8 @@ CREATE TABLE `Clientes` (
   `Funcionários` VARCHAR(50), 
   `Ramo` VARCHAR(150), 
   `Região` VARCHAR(50), 
-  `Observações` LONGTEXT, 
-  INDEX (`CGC`), 
-  INDEX (`Cidade`), 
-  INDEX (`Cep`), 
-  INDEX (`Endereço p/ Cobrança`), 
-  INDEX (`UF`), 
-  INDEX (`Fone`), 
-  INDEX (`IE`), 
-  INDEX (`Empresa`), 
-  INDEX (`Comprador`), 
-  PRIMARY KEY (`Código Cliente`)
-) ENGINE=csv DEFAULT CHARSET=utf8;
+  `Observações` LONGTEXT
+) DEFAULT CHARSET=utf8;
 
 #
 # Dumping data for table 'Clientes'
@@ -50,3 +40,5 @@ CREATE TABLE `Clientes` (
 
 INSERT INTO `Clientes` (`Código Cliente`, `Empresa`, `Status`, `Endereço p/ Faturamento`, `Bairro`, `Cidade`, `UF`, `Cep`, `Endereço p/ Cobrança`, `Bairro p/ Cobrança`, `Cidade p/ Cobrança`, `UF p/ Cobrança`, `Cep p/ Cobrança`, `CGC`, `IE`, `Fone`, `Fax`, `Comprador`, `Ramal`, `Funcionários`, `Ramo`, `Região`, `Observações`) VALUES (1, 'Recrusul S.A', 'A', 'Av. Luis Pasteur, 1020', 'Centro', 'Sapucaia do Sul', 'RS', '93212-360', 'O Mesmo', NULL, NULL, 'RS', NULL, '91333666/0001-17', '132/0003327', '(51) 474-1233', '(51) 474-0000', 'Volmar', NULL, '675', 'Implementos rodoviários', 'Grande Poa', 'Após visita técnica com Sr. Humberto, iniciamos a negociação comercial com Sr. Luchinni, onde este comunicou que utiliza manômetros petroquímico 232.54.115L de 2, 4, 10 e 21 Kg, utiliza termômetros e comunicou que compra St\r\nork IFC 0108-0B de -40 a 50 º em lotes de 50 pç. Solicitou amostra do manômetro e preço do Stork.');
 INSERT INTO `Clientes` (`Código Cliente`, `Empresa`, `Status`, `Endereço p/ Faturamento`, `Bairro`, `Cidade`, `UF`, `Cep`, `Endereço p/ Cobrança`, `Bairro p/ Cobrança`, `Cidade p/ Cobrança`, `UF p/ Cobrança`, `Cep p/ Cobrança`, `CGC`, `IE`, `Fone`, `Fax`, `Comprador`, `Ramal`, `Funcionários`, `Ramo`, `Região`, `Observações`) VALUES (2, 'Pirelli Pneus S.A.', 'BT', 'Rs 030, 1610', 'Parque dos Anjos', 'Gravataí', 'RS', '94180-130', 'O Mesmo', NULL, NULL, 'RS', NULL, '59179838/0003-07', '057/0077273', '(51) 484-1188', '(51)484-1866', 'Márcio', NULL, '885', 'Artefatos de borracha - pneus e câmaras', 'Grande Poa', NULL);
+
+SELECT * FROM clientes;
